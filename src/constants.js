31381b9a5cf2f2e2ce3374f7dfc76c0d4d8ca55f
Fritzzzz <friedmanliran@gmail.com>
2023-01-23 17:06:49 +0200

    added constants file

---
+++ src/constants.js
@@ -0,0 +1,24 @@
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
+export const DIGITAL_ASSISTANTS = [
+  {
+    title: "Alexa",
+    handle: "@alexa99",
+    image: AlexaImage,
+    description: "Alexa was created by Amazon and helps you buy things.",
+  },
+  {
+    title: "Cortana",
+    handle: "@cortana32",
+    image: CortanaImage,
+    description: "Cortana is a Microsoft helper tool",
+  },
+  {
+    title: "Siri",
+    handle: "@Siri01",
+    image: SiriImage,
+    description: "Siri was made by Apple and it is kinda dead",
+  },
+];
