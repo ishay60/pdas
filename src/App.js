c8d65786ad5c32e3354153b28c9bccb4945b94c2
Ishay Rosengarten <ishay60@gmail.com>
2023-01-23 16:01:48 +0200

    culomn styling

--- src/App.js
+++ src/App.js
@@ -2,22 +2,31 @@ import ProfileCard from "./ProfileCard";
 import AlexaImage from "./images/alexa.png";
 import CortanaImage from "./images/cortana.png";
 import SiriImage from "./images/siri.png";
+import 'bulma/css/bulma.css'
 //console.log(AlexaImage);
 //console.log(SiriImage);
 
 function App() {
     return (
         <div>
-            <div>
-                Personal Digital Assistant
+            <div>Personal Digital Assistant</div>
+
+            <div className="container">
+                <section className="section">
+                    <div className="culomns">
+                        <div className="column is-3">
+                            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage}/>
+                        </div>
+                        <div className="column is-3">
+                            <ProfileCard title= "Cortana" handle = "@cortana32" image={CortanaImage}/>
+                        </div>
+                        <div className="column is-3">
+                            <ProfileCard title="Siri" handle="@Siri01" image={SiriImage} />                            
+                        </div>
+                    </div>
+                </section>
             </div>
 
-            <img src={AlexaImage}/>
-            <img src={SiriImage}/>
-            <img src={CortanaImage}/>
-            <ProfileCard title= "Alexa" handle = "@alexa99"/>
-            <ProfileCard title= "Cortana" handle = "@cortana32"/>
-            <ProfileCard title= "Siri" handle = "@Siri01"/>
         </div>
     );
 }
