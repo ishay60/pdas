844a06c1f0e2d4f1f5ef4861693ea076f436920e
Ishay Rosengarten <ishay60@gmail.com>
2023-01-23 03:13:13 +0200

    images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+//console.log(AlexaImage);
+//console.log(SiriImage);
 
 function App() {
     return (
@@ -6,6 +11,10 @@ function App() {
             <div>
                 Personal Digital Assistant
             </div>
+
+            <img src={AlexaImage}/>
+            <img src={SiriImage}/>
+            <img src={CortanaImage}/>
             <ProfileCard title= "Alexa" handle = "@alexa99"/>
             <ProfileCard title= "Cortana" handle = "@cortana32"/>
             <ProfileCard title= "Siri" handle = "@Siri01"/>
